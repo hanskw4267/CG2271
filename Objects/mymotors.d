baa6357f608673c9c58763a02bedce12482ebbb3
Daryl Tan <tanyixiang16@yahoo.com>
2020-03-23 17:36:13 +0800

    Add LED Stuff

--- Objects/mymotors.d
+++ Objects/mymotors.d
@@ -1,9 +1,9 @@
 .\objects\mymotors.o: myMOTORS.c
-.\objects\mymotors.o: C:\Users\Hans Kurnia\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
+.\objects\mymotors.o: C:\Users\Daryl Tan\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
 .\objects\mymotors.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\mymotors.o: C:\Users\Hans Kurnia\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm0plus.h
-.\objects\mymotors.o: C:\Users\Hans Kurnia\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
-.\objects\mymotors.o: C:\Users\Hans Kurnia\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
-.\objects\mymotors.o: C:\Users\Hans Kurnia\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\mymotors.o: C:\Users\Daryl Tan\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\mymotors.o: C:\Users\Daryl Tan\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\mymotors.o: C:\Users\Daryl Tan\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\mymotors.o: C:\Users\Daryl Tan\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
 .\objects\mymotors.o: .\RTE\Device\MKL25Z128xxx4\system_MKL25Z4.h
 .\objects\mymotors.o: myMOTORS.h
